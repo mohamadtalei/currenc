9d94ba84e59e06c3cd3d2ead4a4ae68d77ad21c2
Mohamad Talei <mohamadtalei02@gmail.com>
2021-12-30 14:35:36 +0330

    pwa support

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+const cacheName = 'site-static';
+const assets = ['/fallback.html'];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            cache.addAll(assets);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== cacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request).then(fetchRes => {
+            return fetchRes;
+        }).catch(() =>
+            // caches.match('/fallback.html')
+            console.log("failed")
+        )
+    );
+});
